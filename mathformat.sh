d8018d34e713d1b72e2bf1f0f3ab8ee5575f8d3e
douxl5516 <562125370@qq.com>
2019-07-03 16:10:04 +0800

    7.3

--- mathformat.sh
+++ mathformat.sh
@@ -1,5 +1,7 @@
+yarn remove hexo-renderer-marked
+yarn add hexo-renderer-kramed
 yarn remove hexo-math
 yarn add hexo-renderer-mathjax
-cp backup/inline.js node_modules/kramed/lib/rules/inline.js
-cp backup/mathjax.html node_modules/hexo-renderer-mathjax/mathjax.html
-cp backup/renderer.js node_modules/hexo-renderer-kramed/lib/renderer.js
+\cp backup/inline.js node_modules/kramed/lib/rules/inline.js
+\cp backup/mathjax.html node_modules/hexo-renderer-mathjax/mathjax.html
+\cp -r backup/renderer.js node_modules/hexo-renderer-kramed/lib/renderer.js
